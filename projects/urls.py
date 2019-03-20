646311ea5b7a002aae96468f6cea9a20eb5b2fc6
cinston <wagawc@gmail.com>
2019-03-20 10:30:01 +0300

    add a signup view in views.py

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,13 @@
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url,include
 from . import views
 
+
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url(r'^home/$',views.home_index, name="homePage"),
+    url(r'^$', views.signup),
+    url(r'accounts/', include('django.contrib.auth.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-from django.http  import HttpResponse
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
+from .forms import *
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 
-def welcome(request):
-    return HttpResponse('Iam Groot')
+
+def signup(request):
+    if request.method=='POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+        return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request,'signup.html',locals())
