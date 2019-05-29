05ba027dcf88b6c8ab0d0b04ec395ebfae0bd22f
cinston <wagawc@gmail.com>
2019-05-29 17:41:36 +0300

    add new files and folders

--- awards/urls.py
+++ awards/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('projects.urls')),
-    url('^accounts/', include('registration.backends.simple.urls')),
+    # url('^accounts/', include('registration.backends.simple.urls')),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from .models import *
 from .forms import *
